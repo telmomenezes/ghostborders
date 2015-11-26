029fab01ce17d0f79cd93011ad2280ba46ec34e5
telmomenezes <telmo@telmomenezes.com>
2015-11-26 12:18:22 +0100

    fixing bug in confmodel

--- ghostb/confmodel.py
+++ ghostb/confmodel.py
@@ -87,6 +87,7 @@ def read_graph(csv_path):
 
     g = {}
     for line in lines[1:]:
+        print('LINE: %s' % line)
         tokens = [int(round(float(x))) for x in line.split(',')]
         print('TOKENS: %s' % tokens)
         add_edge(g, [tokens[0], tokens[1]], tokens[2])
