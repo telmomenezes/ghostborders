8e5656e8c02d974047257f29efc9f23a3192eb0a
telmomenezes <telmo@telmomenezes.com>
2015-12-16 18:08:53 +0100

    fixing bug with confmodel

--- ghostb/confmodel.py
+++ ghostb/confmodel.py
@@ -2,7 +2,7 @@ import ghostb.graph as graph
 
 
 def normalize_with_confmodel(csv_in, csv_out, runs):
-    graph = graph.read_graph(csv_in)
-    ref_graph = graph.conf_model_n_times(graph, runs)
-    graph.normalize(graph, ref_graph)
-    graph.write_graph(graph, csv_out)
+    g = graph.read_graph(csv_in)
+    ref_graph = graph.conf_model_n_times(g, runs)
+    graph.normalize(g, ref_graph)
+    graph.write_graph(g, csv_out)
