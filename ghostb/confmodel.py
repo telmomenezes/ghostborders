19ccdf3081795fabaefed9853a793883bc70c139
telmomenezes <telmo@telmomenezes.com>
2016-01-04 14:08:33 +0100

    removing obsolete confmodel strategies

--- ghostb/confmodel.py
+++ ghostb/confmodel.py
@@ -2,23 +2,10 @@ import ghostb.graph as graph
 import math
 
 
-def normalize1(g):
+def normalize(g):
     degs = graph.degrees(g)
     m = len(g) * 2.
-    
-    for edge in g:
-        weight = math.ceil(float(g[edge]))
-        expected = (float(degs[edge[0]]) * float(degs[edge[1]])) / m
-        ref_weight = math.ceil(expected)
-        weight /= ref_weight
-        weight = math.ceil(weight)
-        g[edge] = weight
 
-
-def normalize2(g):
-    degs = graph.degrees(g)
-    m = len(g) * 2.
-    
     for edge in g:
         weight = float(g[edge])
         expected = (float(degs[edge[0]]) * float(degs[edge[1]])) / m
@@ -26,19 +13,7 @@ def normalize2(g):
         g[edge] = weight
 
 
-def normalize3(g):
-    degs = graph.degrees(g)
-    m = len(g) * 2.
-    
-    for edge in g:
-        weight = float(g[edge])
-        expected = (float(degs[edge[0]]) * float(degs[edge[1]])) / m
-        weight -= expected
-        g[edge] = weight
-
-
 def normalize_with_confmodel(csv_in, csv_out, runs):
     g = graph.read_graph(csv_in)
-    #ref_graph = graph.conf_model_n_times(g, runs)
-    normalize2(g)
+    normalize(g)
     graph.write_graph(g, csv_out)
