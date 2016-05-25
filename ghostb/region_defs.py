ff130947afa943d161be1df152f0625f9c7a7803
telmomenezes <telmo@telmomenezes.com>
2016-05-25 15:45:48 +0200

    adding license

--- ghostb/region_defs.py
+++ ghostb/region_defs.py
@@ -1,3 +1,25 @@
+#   Copyright (c) 2016 Centre Marc Bloch Berlin
+#   (An-Institut der Humboldt Universität, UMIFRE CNRS-MAE).
+#   All rights reserved.
+#
+#   Written by Telmo Menezes <telmo@telmomenezes.com>
+#
+#   This file is part of GhostBorders.
+#
+#   GhostBorders is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   GhostBorders is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with GhostBorders.  If not, see <http://www.gnu.org/licenses/>.
+
+
 region_coords = {
     'israel': [29.340105, 33.736538, 33.446167, 36.098598],
     'belgium': [49.4, 2.420480, 51.726419, 6.589545],
--- ghostb/scales_vis.py
+++ ghostb/scales_vis.py
@@ -1,3 +1,25 @@
+#   Copyright (c) 2016 Centre Marc Bloch Berlin
+#   (An-Institut der Humboldt Universität, UMIFRE CNRS-MAE).
+#   All rights reserved.
+#
+#   Written by Telmo Menezes <telmo@telmomenezes.com>
+#
+#   This file is part of GhostBorders.
+#
+#   GhostBorders is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   GhostBorders is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with GhostBorders.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from ghostb.scales import Scales
 from ghostb.cropborders import CropBorders
 from ghostb.draw_map import draw_map
